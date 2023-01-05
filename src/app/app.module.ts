d42063605f9ce17cdcaddf99fdd8b373b5bf5bbc
AntonyMadaleno <55442410+AntonyMadaleno@users.noreply.github.com>
2023-01-05 16:37:32 +0100

    Add files via upload
    seulement src, login/register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CardComponent } from './card/card.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginRegistrationComponent } from './login-registration/login-registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CardComponent
+    CardComponent,
+    LoginComponent,
+    RegisterComponent,
+    LoginRegistrationComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/login-registration/login-registration.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginRegistrationComponent } from './login-registration.component';
+
+describe('LoginRegistrationComponent', () => {
+  let component: LoginRegistrationComponent;
+  let fixture: ComponentFixture<LoginRegistrationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LoginRegistrationComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LoginRegistrationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
